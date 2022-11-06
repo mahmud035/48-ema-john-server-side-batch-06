213466b51a72fb4c27c9a414a9709a072b45e5dd
mahmud035 <mahamudulhasan4148@gmail.com>
2022-11-06 09:44:05 +0600

    basic server setup and connected to mongodb atlas

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+require('colors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//* Middleware
+app.use(cors());
+app.use(express.json());
+
+//* Mongodb Atlas
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yeflywl.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+    console.log('Database connected'.yellow.italic);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+};
+
+dbConnect();
+
+app.get('/', (req, res) => {
+  res.send('Ema John Server is running');
+});
+
+app.listen(port, () => {
+  console.log('Server up and running'.cyan.bold);
+});
